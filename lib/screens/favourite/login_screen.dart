5ab73e669327d4ddac764297a138fc763e86b5b1
Rauf Junaid <roafjunaid09@gmail.com>
2026-06-14 13:43:46 +0500

    'login'

--- lib/screens/favourite/login_screen.dart
+++ lib/screens/favourite/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:login_with_rest_api_with_provider/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,17 +10,128 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  
+  TextEditingController email = TextEditingController();
+  TextEditingController passwword = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
     return  Scaffold(
       appBar:AppBar(
-        backgroundColor: Colors.teal.shade400,
+        backgroundColor: Colors.deepOrange,
         title: Text("Login"),
       ) ,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: email,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: "Enter your Email",
+                suffixIcon: Icon(Icons.email_outlined),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                      color: Colors.grey,
+                      width: 2
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.grey.shade100,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your Email';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextFormField(
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              controller: passwword,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: "Enter your Password",
+                suffixIcon: Icon(Icons.password),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                      color: Colors.grey,
+                      width: 2
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.grey.shade100,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your Password';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20
+            ),
+            child: InkWell(
+              onTap: (){
+                authProvider.login(email.text.toString(),
+                passwword.text.toString()
+                );
+              },
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.circular(20)
+                ),
+                child: Center(
+                  child:authProvider.loading ? CircularProgressIndicator(
+                    color: Colors.white,
+                  ) : Text("Login"),
+
+                ),
+              ),
+            ),
+          )
 
         ],
       ),
